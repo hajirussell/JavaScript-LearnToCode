ff4c659ffd8cb97b96d3fddb870e41a16b5b65ee
Haji Russell <hajirussell@gmail.com>
2021-05-18 10:06:02 -0400

    added demo1

---
+++ Mod-02-JavaScript-Basics/Demo1/myfirst.js
@@ -0,0 +1,18 @@
+"use strict";
+
+let firstName = "Haji";
+let lastName = "Russell";
+
+let age = 28;
+
+console.log(lastName + "," + firstName);       //ex; Russell, Haji
+console.log("My name is " + firstName + " " + lastName + " and I am" + age);     //ex: My name is Haji Russell
+
+let address;
+let city, state;
+
+/*
+  <input type="text" id="city">
+
+  city = document.getElementById("city").value;
+*/
